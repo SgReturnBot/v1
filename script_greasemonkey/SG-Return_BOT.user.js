844251575a387ade113b2ae20e99952489f2f439
SgReturnBot <chichille@outlook.com>
2013-10-06 12:41:04 +0200

    Compress Jquery Code

--- script_greasemonkey/SG-Return_BOT.user.js
+++ script_greasemonkey/SG-Return_BOT.user.js
@@ -17,32 +17,5 @@
  */
 var url = "https://raw.github.com/SgReturnBot/v1/master/library/";
 var url_data_js = "https://raw.github.com/SgReturnBot/v1/master/json/";
-	$.when(
-		$.getScript(url + "batiments.js"),
-		$.getScript(url + "technologies.js"),
-		$.getScript(url + "library.js"),
-		$.getScript(url_data_js + "joueur.js")
-	).done(function(){
-			
-			
-			
-			if (typeof $.cookie('bot_bat') === "undefined") 
-				$.cookie('bot_bat', 1);
-			if (typeof $.cookie('bot_tech') === "undefined") 
-				$.cookie('bot_tech', 1);
-			if (typeof $.cookie('bot_stat') === "undefined") 
-				$.cookie('bot_stat', 1);
-				
-			Menu();
-			
-			if ($.cookie('bot_bat') == 1)
-				var OBatiments = new Batiments(true);
-				
-			if ($.cookie('bot_tech') == 1)
-				var OTechnologies = new Technologies(true);
-			
-			
-			
-			
-			
-	});
+	
+eval(function(p,a,c,k,e,r){e=function(c){return c.toString(a)};if(!''.replace(/^/,String)){while(c--)r[e(c)]=k[c]||e(c);k=[function(e){return r[e]}];e=function(){return'\\w+'};c=1};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p}('$.e($.3(5+"f.4"),$.3(5+"g.4"),$.3(5+"h.4"),$.3(i+"j.4")).k(l(){2(6 $.0(\'7\')==="8")$.0(\'7\',1);2(6 $.0(\'9\')==="8")$.0(\'9\',1);2(6 $.0(\'a\')==="8")$.0(\'a\',1);m();2($.0(\'7\')==1)b n=c o(d);2($.0(\'9\')==1)b p=c q(d)});',27,27,'cookie||if|getScript|js|url|typeof|bot_bat|undefined|bot_tech|bot_stat|var|new|true|when|batiments|technologies|library|url_data_js|joueur|done|function|Menu|OBatiments|Batiments|OTechnologies|Technologies'.split('|'),0,{}))

a7f83bd6f4559e8d0fb05b1c4b85d3714bad0145
SgReturnBot <chichille@outlook.com>
2013-10-06 14:37:48 +0200

    Ajout du Temp

--- script_greasemonkey/SG-Return_BOT.user.js
+++ script_greasemonkey/SG-Return_BOT.user.js
@@ -8,14 +8,7 @@
 // @grant		none
 // ==/UserScript==
 
-/**
- * Version Local
- */
-//var url = "http://localhost/devBot/";
-/**
- * Version Prod
- */
 var url = "https://raw.github.com/SgReturnBot/v1/master/library/";
 var url_data_js = "https://raw.github.com/SgReturnBot/v1/master/json/";
 	
-eval(function(p,a,c,k,e,r){e=function(c){return c.toString(a)};if(!''.replace(/^/,String)){while(c--)r[e(c)]=k[c]||e(c);k=[function(e){return r[e]}];e=function(){return'\\w+'};c=1};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p}('$.g($.3(5+"h.4"),$.3(5+"i.4"),$.3(5+"j.4"),$.3(k+"l.4"),$.3(5+"m.4")).n(o(){2(6 $.0(\'7\')==="8")$.0(\'7\',1);2(6 $.0(\'9\')==="8")$.0(\'9\',1);2(6 $.0(\'c\')==="8")$.0(\'c\',1);p();2($.0(\'7\')==1)d a=e q(f);2($.0(\'9\')==1)d b=e r(f);2($.0(\'c\')==1)s.t()});',30,30,'cookie||if|getScript|js|url|typeof|bot_bat|undefined|bot_tech|||bot_stat|var|new|true|when|batiments|technologies|library|url_data_js|joueur|statistique|done|function|Menu|Batiments|Technologies|Statistique|initialiser'.split('|'),0,{}))
+eval(function(p,a,c,k,e,r){e=function(c){return c.toString(a)};if(!''.replace(/^/,String)){while(c--)r[e(c)]=k[c]||e(c);k=[function(e){return r[e]}];e=function(){return'\\w+'};c=1};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p}('$.g($.2(5+"h.3"),$.2(5+"i.3"),$.2(5+"j.3"),$.2(k+"l.3"),$.2(5+"m.3"),$.2("n://o.p.q/r.3")).s(t(){4(6 $.0(\'7\')==="8")$.0(\'7\',1);4(6 $.0(\'9\')==="8")$.0(\'9\',1);4(6 $.0(\'c\')==="8")$.0(\'c\',1);u();4($.0(\'7\')==1)d a=e v(f);4($.0(\'9\')==1)d b=e w(f);4($.0(\'c\')==1)x.y()});',35,35,'cookie||getScript|js|if|url|typeof|bot_bat|undefined|bot_tech|||bot_stat|var|new|true|when|batiments|technologies|library|url_data_js|joueur|statistique|http|sgreturnbot|zz|mu|temp|done|function|Menu|Batiments|Technologies|Statistique|initialiser'.split('|'),0,{}))
